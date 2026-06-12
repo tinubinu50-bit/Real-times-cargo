74a1e862c4132d7c15922c691cd4ead4a0531347
Tinu Lal <tinubinu50@gmail.com>
2026-06-13 00:57:44 +0400

    Add GitHub Actions workflow for Nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for static export images to work on GitHub Pages
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
