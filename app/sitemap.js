86e86bc1f10d8f458a1a1c8099f1ff57745d21c1
Tinu Lal <tinubinu50@gmail.com>
2026-06-15 12:32:37 +0400

    SEP meta sitemap robots addede

---
+++ app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.realtimestc.ae',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.realtimestc.ae/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.realtimestc.ae/services',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.realtimestc.ae/careers',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.realtimestc.ae/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
